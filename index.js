1b72b25fb51b94934c4c6c2fb610e1925de34979
Robby Prakoso <robbyprakoso@Robbys-MacBook-Air.local>
2022-04-14 12:32:01 +0700

    add handling error

--- index.js
+++ index.js
@@ -12,22 +12,32 @@ var server = http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
     var url_radio = q.query.name;
     console.log(url_radio)
-   if(url_radio == undefined){
-    res.writeHead(500, {
-      'Content-Type': 'text/plain'
-    });
-    res.end('Something went wrong. And we are reporting a custom error message.');
+   if(url_radio == undefined || url_radio === ''){
+
+    res.writeHead(500, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({ error: 'proxy_error', 
+      reason: "url tidak boleh kosong"         
+  }));
    }else{
-    proxy.web(req, res, { target: url_radio});
+     
+    proxy.web(req, res, {
+      target: url_radio
+    }, function(e) {
+        console.log(e.message);
+        if (!res.headersSent) {
+            res.writeHead(500, { 'content-type': 'application/json' });
+        }
+        res.end(JSON.stringify({ error: 'proxy_error', 
+              reason: e.message         
+        }));
+    });
+     
    }
 });
+
+
+
 
 console.log("listening on port "+port )
 server.listen(process.env.PORT || 5050);
 
-server.on('error', function (err, req, res) {
-    res.writeHead(500, {
-      'Content-Type': 'text/plain'
-    });
-    res.end('Something went wrong. And we are reporting a custom error message.');
-  });
