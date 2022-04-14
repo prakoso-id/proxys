5afa1ba6bfb5ae84c869b288834f30fdc09c9538
Robby Prakoso <robbyprakoso@Robbys-MacBook-Air.local>
2022-04-14 08:59:23 +0700

    change index

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ var server = http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
     var url_radio = q.query.name;
     console.log(url_radio)
-   if(url_radio != ''|| url_radio != undefined){
+   if(url_radio == undefined){
+    res.writeHead(500, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('Something went wrong. And we are reporting a custom error message.');
+
+
+   }else{
     proxy.web(req, res, { target: url_radio});
+
    }
 });
 
